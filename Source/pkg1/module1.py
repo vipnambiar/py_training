2bfb2906b11ac95a70c96366fd911fecc9fe3e4a
Vipin Nambiar Balakrishnan <bvipin@zeomega.com>
2017-09-13 14:16:24 +0530

    Updated files

--- Source/pkg1/module1.py
+++ Source/pkg1/module1.py
@@ -1,9 +1,11 @@
-import pkg2
+#import pkg2
 
 
-def add(a,b):
-    a = a + 10
-    return a+b
+def add():
+    first_val = raw_input("Enter first val:")
+    second_val = raw_input("Enter second val:")
+    import pdb; pdb.set_trace()
+    return int(first_val) + int(second_val)
 
 if __name__ == '__main__':
-    add(10, 20)
+    print add()
--- Source/pkg2/module2.py
+++ Source/pkg2/module2.py
@@ -1,10 +1,12 @@
 import sys
-from pkg1 import module1
+from pkg1 import module1 as md
 
 
 def hello():
+    md.add()
     print "namespace:", __name__
 
 
 if __name__ == '__main__':
+    args = sys.argv
     hello()
