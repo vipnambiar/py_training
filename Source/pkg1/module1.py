062e6d39ea5550ea383312221b297b0bff381c60
Vipin Nambiar Balakrishnan <bvipin@zeomega.com>
2018-10-17 16:13:32 +0530

    Updated as on 16th Oct 2018

--- Source/pkg1/module1.py
+++ Source/pkg1/module1.py
@@ -1,8 +1,10 @@
 import pkg2.module2 as md2
+from pkg2.module2 import hello
+
 
 
 def add():
-    md2.hello(__name__)
+    hello(__name__)
     first_val = raw_input("Enter first val:")
     second_val = raw_input("Enter second val:")
     return int(first_val) + int(second_val)
